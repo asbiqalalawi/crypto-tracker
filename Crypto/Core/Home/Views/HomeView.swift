bd0dd55ecb272e2de8f99b5ca4757193a01e7654
Asbiq Al Alawi <asbiqalalawi@gmail.com>
2023-12-13 21:20:03 +0700

    feat(ui): Add a dynamic List to display downloaded coins

--- Crypto/Core/Home/Views/HomeView.swift
+++ Crypto/Core/Home/Views/HomeView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct HomeView: View {
     
+    @EnvironmentObject private var vm: HomeViewModel
     @State private var showPortfolio: Bool = false
     
     var body: some View {
@@ -19,15 +20,27 @@ struct HomeView: View {
             // content layer
             VStack {
                 homeHeader
+                
+                columnTitles
+                
+                if showPortfolio {
+                    portfolioCoinsList
+                } else {
+                    allCoinsList
+                }
+                
                 Spacer()
             }
         }
     }
 }
 
-#Preview {
-    NavigationStack {
-        HomeView()
+struct HomeView_Preveiw: PreviewProvider {
+    static var previews: some View {
+        NavigationStack {
+            HomeView()
+                .environmentObject(dev.vm)
+        }
     }
 }
 
@@ -54,6 +67,43 @@ extension HomeView {
                     }
                 }
         }
+        .padding(.horizontal)
+    }
+    
+    private var portfolioCoinsList: some View {
+        List {
+            ForEach(vm.portfolioCoins) { coin in
+                CoinRowView(coin: coin, showHoldingColumn: true)
+                    .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
+            }
+        }
+        .listStyle(.plain)
+        .transition(.move(edge: .trailing))
+    }
+    
+    private var allCoinsList: some View {
+        List {
+            ForEach(vm.allCoins) { coin in
+                CoinRowView(coin: coin, showHoldingColumn: false)
+                    .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
+            }
+        }
+        .listStyle(.plain)
+        .transition(.move(edge: .leading))
+    }
+    
+    private var columnTitles: some View {
+        HStack {
+            Text("Coin")
+            Spacer()
+            if showPortfolio {
+                Text("Holdings")
+            }
+            Text("Price")
+                .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
+        }
+        .font(.caption)
+        .foregroundStyle(Color.theme.secondaryTextColor)
         .padding(.horizontal)
     }
 }
--- Crypto/Extensions/PreviewProvider.swift
+++ Crypto/Extensions/PreviewProvider.swift
@@ -21,6 +21,8 @@ class DeveloperPreview {
     static let instance = DeveloperPreview()
     private init() {}
     
+    let vm = HomeViewModel()
+    
     let coin = CoinModel(
         id: "bitcoin",
         symbol: "btc",
